ef6e912d2ad0816fe85a1fe9b9d39fb44982f70a
SHINING <benjaminndjengshining@gmail.com>
2026-05-20 16:37:15 +0100

    Update api.js

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: `${import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000'}/api`,
   headers: { 'Content-Type': 'application/json' }
 });
 
